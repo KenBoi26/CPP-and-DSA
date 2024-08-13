195c29a9be70ab30de9351497f25c2854328a6e8
KenBoi26 <kennethphilip23@lpu.in>
2024-08-13 20:49:51 +0530

    introduction to cpp

---
+++ C++/test.cpp
@@ -0,0 +1,9 @@
+#include<iostream> // compulsory
+using namespace std; // used so that we don't have to do std:: everytime
+
+int main(){
+    cout << "Hello World" << endl;
+    // cout << "Hey"; ctrl+/ is used in commenting an entire line
+
+    return 0; // optional in the main function
+}
