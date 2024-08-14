76ada6410ee67b58b3f29b5a9cc0204a08fdaa33
KenBoi26 <kennethphilip23@lpu.in>
2024-08-14 19:10:50 +0530

    chore: Remove commented code and add newline at end of file in test.cpp

--- C++/test.cpp
+++ C++/test.cpp
@@ -3,7 +3,8 @@ using namespace std; // used so that we don't have to do std:: everytime
 
 int main(){
     cout << "Hello World" << endl;
-    // cout << "Hey"; ctrl+/ is used in commenting an entire line
+    // cout << "Hey"; 
+    // ctrl+/ is used in commenting an entire line
 
     return 0; // optional in the main function
 }
