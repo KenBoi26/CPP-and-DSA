7731459580cd60c6daf11bc812ff0e37d187ac6b
Kenneth <kennethphilip23@lpu.in>
2024-08-20 19:35:11 +0530

    chore: Add nested for loop and arithmetic operators in C++

---
+++ C++/calculator.cpp
@@ -0,0 +1,28 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a;
+    cin >> b;
+    
+    char oper;
+    cin >> oper;
+    
+    if(oper == '+'){
+        cout << a+b;
+    }else if(oper == '-'){
+        cout << a-b;
+    }else if(oper == '*'){
+        cout << a*b;
+    }else if(oper == '/'){
+        if(b==0){
+            cout << "Invalid input";
+        }else{
+            cout << a/b;
+        }
+    }
+
+    return 0;
+}
---
+++ C++/operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Arithmetic operators
+
+    int a = 5;
+    int b = 10;
+
+    int ans = a+b;
+
+    cout << ans << endl;
+
+    return 0;
+
+}
