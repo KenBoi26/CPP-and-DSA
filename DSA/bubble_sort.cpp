f6dd8a4d945d9ec5654396f4b2adffe7ec9e2d90
Kenneth <kennethphilip23@lpu.in>
2024-08-29 18:37:39 +0530

    bubble sorting

--- DSA/bubble_sort.cpp
+++ DSA/bubble_sort.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n){
-    for (int i = 0; i < n-1; i++){
-        for (int j = 0; j < n -i - 1; j++){
+    for (int i = n-1; i >= 1; i--){
+        for (int j = 0; j < i - 1; j++){
             if (arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
@@ -11,4 +11,15 @@ void bubbleSort(int arr[], int n){
             }
         }
     }
+}
+
+int main(){
+    int arr[10] = {5,7,3,2,9};
+
+    bubbleSort(arr,5);
+
+    for (int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
