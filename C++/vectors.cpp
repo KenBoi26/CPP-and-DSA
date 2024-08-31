a9278b5075a758aa95dba21188964026aacc40be
Kenneth <kennethphilip23@lpu.in>
2024-08-31 11:37:53 +0530

    Refactor code for improved readability and user input

--- C++/vectors.cpp
+++ C++/vectors.cpp
@@ -6,6 +6,12 @@ int main(){
 
     vector<int> vec1 = {1,2,3,4,5};
 
+    vec1.push_back(10);
+    
+    // view item at an index
+    cout << vec1[0] << endl;
+    vec1.at(1);
+
     for(int i : vec1){
         cout << i << " ";
     }
