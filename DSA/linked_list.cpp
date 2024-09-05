2f99783ce58300efad585cf5be70393a5562f5a1
Kenneth <kennethphilip23@lpu.in>
2024-09-05 19:40:48 +0530

    linked list

--- DSA/linked_list.cpp
+++ DSA/linked_list.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 
 class Node{
+    public:
     int data;
     Node* next;
 
@@ -14,12 +15,43 @@ class Node{
 };
 
 
+void insertAtTail(Node* &head, int val){
+    Node* n = new Node(val);
 
+    if (head == NULL) {
+        head = n;
+    } else {
+        Node* temp = head;
+
+        while(temp->next != NULL){
+            temp = temp->next;
+        }
+
+        temp->next = n;
+    }
+}
+
+void display(Node* head){
+    Node* temp = head;
+    
+    while (temp!=NULL){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
+    
+}
 
 
 int main(){
 
     Node* head = NULL;
+
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+
+    display(head);
 
 
     return 0;
