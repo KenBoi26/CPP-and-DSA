50a225d0575652661f0c29d1349be69cbd82acd0
Kenneth <kennethphilip23@lpu.in>
2024-09-05 19:17:05 +0530

    linked list

---
+++ DSA/linked_list.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+class Node{
+    int data;
+    Node* next;
+
+    Node(int val){
+        this->data = val;
+        next = NULL;
+    }
+
+};
+
+
+
+
+
+int main(){
+
+    Node* head = NULL;
+
+
+    return 0;
+}
