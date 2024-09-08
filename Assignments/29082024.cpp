56311d8cd5fc2cd81af2e946381ad64fe78f343e
Kenneth <kennethphilip23@lpu.in>
2024-09-08 19:24:57 +0530

    finding the index of a target element in the array

--- Assignments/29082024.cpp
+++ Assignments/29082024.cpp
@@ -28,3 +28,36 @@ int main(){
 
     return 0;
 }
+
+
+// Question 2
+
+#include <iostream>
+using namespace std;
+
+int targetIndex(int arr[], int target, int size){
+    
+
+    for(int i=0; i<size; i++){
+        if(arr[i] == target){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
+int main(){
+    
+    int arr[] = {1,2,3,4,5,6};
+    int target;
+
+    cout << "Target: ";
+    cin >> target;
+
+    cout << targetIndex(arr,target,6);
+    
+    
+    return 0;
+}
