7af744c92e5193814e74dab2a27ec5490c7f1f1c
Kenneth <kennethphilip23@lpu.in>
2024-09-08 18:23:44 +0530

    questions related to pointers

--- Assignments/27082024.cpp
+++ Assignments/27082024.cpp
@@ -14,3 +14,39 @@ int main(){
 
     return 0;
 }
+
+
+// Question 2
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int *p = arr;
+
+    for(int i=0; i<5; i++){
+        cout << *(p + i) << endl;
+    }
+
+    return 0;
+}
+
+
+// Question 3
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+
+    int *p1 = &num;
+    int **p2 = &p1;
+
+    cout << num << endl;
+    cout << *p1 << endl;
+    cout << **p2;
+}
