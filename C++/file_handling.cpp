54cf97fa734d5375f773277502b345653fcb426c
KenBoi26 <kennethphilip23@lpu.in>
2024-09-16 19:48:27 +0530

    Add file handling functionality to read, write, and append to a file

--- C++/file_handling.cpp
+++ C++/file_handling.cpp
@@ -44,3 +44,66 @@ int main(){
 
     file.close();
 }
+
+
+int main(){
+
+    // write to file
+    fstream file("test1.txt", ios::out);
+
+    if(file){
+        file << "testing with line 1" << endl;
+        file.close();
+    }else{
+        cout << "Error in opening file" << endl;
+        return -1;
+    }
+
+
+    // read from file
+
+    string line;
+    file.open("test1.txt", ios::in);
+
+    if(file){
+        while(!file.eof()){
+            getline(file, line);
+            cout << "Read : " << line << endl;
+        }
+    }else{
+        cout << "Error in opening file" << endl;
+        return -1;
+    }
+
+
+    // append
+    file.open("test1.txt", ios::app);
+
+        if(file){
+            file << "testing with line 2" << endl;
+            file.close();
+        }else{
+            cout << "Error i opening file" << endl;
+            return -1;
+        }
+}
+
+
+
+int main(){
+    char name[50] = "Hello";
+    int age = 10;
+
+    ofstream file("binary", ios::binary);
+
+    file.write(name, sizeof(name));
+    file.write(reinterpret_cast<char*> (&age), sizeof(age));
+
+    char name2[50];
+
+    ifstream file2("binary", ios::binary);
+    file2.read(name2, sizeof(name2));
+
+    cout << "Name: " << name2;
+    file.close();
+}
