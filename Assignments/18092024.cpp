d88f060ec7145f021ac8d39eecf4928d752824a6
KenBoi26 <kennethphilip23@lpu.in>
2024-10-01 19:14:36 +0530

    minor changes

--- Assignments/18092024.cpp
+++ Assignments/18092024.cpp
@@ -31,3 +31,77 @@ int main(){
     
     return 0;
 }
+
+
+// Question 2: Implement the stack using linked list
+
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int val){
+        this->data = val;
+        this->next = nullptr;
+    }
+};
+
+class Stack{
+    public:
+    Node* top;
+    Stack(){
+        this->top = nullptr;
+    }
+
+    void push(int val){
+        Node* n = new Node(val);
+        if(top == nullptr){
+            top = n;
+            return;
+        }
+        n->next = top;
+        top = n;
+    }
+
+    void pop(){
+        if(top == nullptr){
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    int peek(){
+        if(top == nullptr){
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+        return top->data;
+    }
+
+    bool isEmpty(){
+        return top == nullptr;
+    }
+
+};
+
+int main(){
+    Stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.peek() << endl;
+    s.pop();
+    cout << s.peek() << endl;
+    cout << s.isEmpty() << endl;
+    s.pop();
+    s.pop();
+    cout << s.isEmpty() << endl;
+    s.pop();
+    return 0;
+}
