7199dea5a34d739be2b4c7284a70444b55bc725d
KenBoi26 <kennethphilip23@lpu.in>
2024-10-08 20:27:32 +0530

    learning about abstraction

---
+++ C++/abstraction.cpp
@@ -0,0 +1,21 @@
+// abstraction -> displaying only essential information and ignoring the details
+
+
+// e.g. car moves when we accelerate and not concerned about the internal working of the engine
+
+
+// Abstraction is one of the key concepts of object-oriented programming (OOP) languages. Its main goal is to handle complexity by hiding unnecessary details from the user. That enables the user to implement more complex logic on top of the provided abstraction without understanding or even thinking about all the hidden complexity.
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int main(){
+
+    int x = 5;
+    cout << pow(2,x);
+
+
+    return 0;
+}
