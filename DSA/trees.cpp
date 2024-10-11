268dba1d3f41534c4a54c222461266fc6e23c424
KenBoi26 <kennethphilip23@lpu.in>
2024-10-11 20:55:39 +0530

    theory on trees

---
+++ DSA/trees.cpp
@@ -0,0 +1,21 @@
+// trees -> non-linear hierarchical data structure
+
+// depth of x -> length of path from root to x OR number of edges from root to x
+
+// height of x -> number of edges in longest path from x to the leaf
+
+// binary tree -> a tree in which each node have atmost 2 children
+
+// complete Binary Tree -> all levels except the last are completely filled and all nodes are as left as possible
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+
+
+
+    return 0;
+}
