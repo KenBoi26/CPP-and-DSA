8b245b303f50bef32f9157566fd12ef3386b3681
KenBoi26 <kennethphilip23@lpu.in>
2024-10-19 19:10:13 +0530

    Made queue using linked lists

---
+++ Assignments/27092024.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+public:
+    Queue() : front(nullptr), rear(nullptr) {}
+
+    void enqueue(int val) {
+        Node* newNode = new Node(val);
+        if (rear == nullptr) {
+            front = rear = newNode;
+            return;
+        }
+        rear->next = newNode;
+        rear = newNode;
+    }
+
+    void dequeue() {
+        if (front == nullptr) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        Node* temp = front;
+        front = front->next;
+        if (front == nullptr) {
+            rear = nullptr;
+        }
+        delete temp;
+    }
+
+    void display() {
+        if (front == nullptr) {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        Node* temp = front;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
