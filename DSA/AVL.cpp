868ac1f1939cb1c402e7c95a9ff9cfead9799798
KenBoi26 <kennethphilip23@lpu.in>
2024-11-26 10:44:14 +0530

    AVL Tree and it's insertion

---
+++ DSA/AVL.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int key;
+    int height;
+    Node* left;
+    Node* right;
+
+    Node(int k){
+        key = k;
+        right = nullptr;
+        left = nullptr;
+        height = 1;
+    }
+};
+
+int height(Node* N){
+    if(N == nullptr){
+        return 0;
+    }
+
+    return N->height;
+}
+
+int max(int a, int b){
+    return (a>b)?a:b;
+}
+
+Node* rightRotate(Node* y){
+    Node* x = y->left;
+    Node* T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = 1 + max(height(y->left), height(y->right));
+    x->height = 1 + max(height(x->left), height(x->right));
+
+    return x;
+}
+
+
+Node* leftRotate(Node *x){
+    Node *y = x->right;
+    Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = 1 + max(height(x->left), height(x->right));
+    y->height = 1 + max(height(y->left), height(y->right));
+
+    return y;
+}
+
+
+int getBalance(Node* N){
+    if(N == nullptr){
+        return 0;
+    }
+
+    return height(N->left) - height(N->right);
+}
+
+
+Node* insert(Node* node, int key){
+    if(node == nullptr){
+        return new Node(key);
+    }
+
+    if(key < node->key){
+        node->left = insert(node->left, key);
+    }else if(key > node->key){
+        node->right = insert(node->right, key);
+    }else{
+        return node;
+    }
+
+    node->height = 1 + max(height(node->left), height(node->right));
+
+
+    int balance = getBalance(node);
+
+    if(balance > 1 && key < node->left->key){
+        return rightRotate(node);
+    }
+
+    if(balance < -1 && key > node->right->key){
+        return leftRotate(node);
+    }
+
+    if(balance > 1 && key > node->left->key){
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if(balance < -1 && key < node->right->key){
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+
+    return node;
+}
+
+
+void preOrder(Node *root){
+    if(root != nullptr){
+        cout << root->key << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+
+int main(){
+    
+    Node *root = nullptr;
+    root = insert(root, 10);
+    root = insert(root, 20);
+    root = insert(root, 30);
+    root = insert(root, 40);
+    root = insert(root, 50);
+    root = insert(root, 25);
+
+    
+    cout << "Preorder traversal of the constructed AVL tree is \n";
+    preOrder(root);
+    
+    return 0;
+}
